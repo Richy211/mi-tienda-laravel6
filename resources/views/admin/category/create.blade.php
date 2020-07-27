e6ebea5be895377c4f1bd0b5d8223237cedd1e31
Ricardo <ricardollanos1@gmail.com>
2020-07-27 02:43:04 -0400

    Cambie el titulo y agregue un parrafo

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -20,8 +20,8 @@
       <!-- Default box -->
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Administracion de Categorias </h3>
-
+          <h3 class="card-title">Cambiando las categorias </h3>
+  <p>Hola mundo desde VS code</p>
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
               <i class="fas fa-minus"></i></button>
