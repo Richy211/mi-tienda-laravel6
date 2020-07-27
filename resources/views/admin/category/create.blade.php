74005c6a05c8bb2222b1243f69a1fa5eda32fa30
Ricardo <ricardollanos1@gmail.com>
2020-07-27 02:36:54 -0400

    Borre una etiqueta de parrafo

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -21,7 +21,7 @@
       <div class="card">
         <div class="card-header">
           <h3 class="card-title">Administracion de Categorias </h3>
-  <p>Hola saludos</p>
+
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
               <i class="fas fa-minus"></i></button>
